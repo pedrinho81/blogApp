a87f0d66fc13852d8cf106926d23682af068d260
unknown <1pedrohenri@gmail.com>
2021-07-01 16:24:56 -0300

    deletCategoria

--- routes/admin.js
+++ routes/admin.js
@@ -81,6 +81,16 @@ router.get("/categorias/edit/:id", (req, res) => {
     
 })
 
+router.post("/categorias/delete/", (req, res) => {
+    Categoria.deleteOne({_id: req.body.idDelete}).then(() => {
+        req.flash("success_msg", "Categoria deletada com sucesso!")
+        res.redirect("/admin/categorias")
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao deletar a categoria")
+        res.redirect("/admin/categorias")
+    })
+})
+
 router.post("/categorias/edit", (req, res) => {
         
         Categoria.findOne({_id: req.body.id}).then((categoria) => {
